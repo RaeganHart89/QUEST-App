d5c8daeb86334d3157702a33b267254b40c671b9
Danielle Vass <danielle@de-velopment.com>
2015-10-28 17:43:09 +0000

    added question generator screen

--- MyApplication/app/src/main/java/questionapp/rharttech/dev/com/questapp/QuestionGeneratorActivity.java
+++ MyApplication/app/src/main/java/questionapp/rharttech/dev/com/questapp/QuestionGeneratorActivity.java
@@ -4,36 +4,68 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
 
 
 public class QuestionGeneratorActivity extends ActionBarActivity {
+
+    private ArrayAdapter<CharSequence> adapterQuantifierUpper;
+    private ArrayAdapter<CharSequence> adapterQuantifierLower;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_generator);
-    }
+
+        //verb spinner
+        Spinner spinnerVerb = (Spinner) findViewById(R.id.spinnerVerb);
+        ArrayAdapter<CharSequence> adapterVerb = ArrayAdapter.createFromResource(this,
+                R.array.array_verbs, android.R.layout.simple_spinner_item);
+        adapterVerb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerVerb.setAdapter(adapterVerb);
+
+        //quantities spinner
+        final Spinner spinnerQuantifier = (Spinner) findViewById(R.id.spinnerQuantifier);
+
+         adapterQuantifierUpper = ArrayAdapter.createFromResource(this,
+                R.array.array_quantifier_upper, android.R.layout.simple_spinner_item);
+        adapterQuantifierUpper.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+       adapterQuantifierLower = ArrayAdapter.createFromResource(this,
+                R.array.array_quantifier_lower, android.R.layout.simple_spinner_item);
+        adapterQuantifierLower.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //set it to be lower to start with
+        spinnerQuantifier.setAdapter(adapterQuantifierLower);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_question_generator, menu);
-        return true;
-    }
+        spinnerVerb.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+              //work out whether to show the upper or lower quantifiers
+                if(i < 10){
+                    //set to be lower
+                    spinnerQuantifier.setAdapter(adapterQuantifierLower);
+                } else {
+                    //set to be higher
+                    spinnerQuantifier.setAdapter(adapterQuantifierUpper);
+                }
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //don't worry here
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+
+
+
     }
 }
